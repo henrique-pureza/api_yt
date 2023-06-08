c48c4d22b101455100c14807eeec38e96d2400c8
Henrique Pureza Queiroz <henrique.pureza.queiroz@gmail.com>
2023-06-08 16:40:58 -0300

    a

--- routes.py
+++ routes.py
@@ -8,15 +8,15 @@ CORS(app)
 @app.route("/", methods=["GET", "POST"])
 def dl():
     if request.method == "POST":
-        try:
-            req     = request.get_json()
-            url     = req["url"]
-            path    = download(url)
+        # try:
+        req     = request.get_json()
+        url     = req["url"]
+        path    = download(url)
 
-            return make_response(send_file(path, as_attachment=True), 200)
-        except:
-            return make_response(jsonify({"error": "No valid URL was given. \
-Make a JSON POST request with an 'url' key with the URL of your YouTube video."}), 400)
+        return make_response(send_file(path, as_attachment=True), 200)
+        # except:
+            # return make_response(jsonify({"error": "No valid URL was given. \
+# Make a JSON POST request with an 'url' key with the URL of your YouTube video."}), 400)
     else:
         return make_response(jsonify({"error": "405 method not allowed. Just make POST requests."}), 405)
 
