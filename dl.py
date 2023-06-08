5fcd210aa0d319ae35fd203b8107de69b4d517f7
Henrique Pureza Queiroz <henrique.pureza.queiroz@gmail.com>
2023-06-08 17:02:25 -0300

    Final version

--- dl.py
+++ dl.py
@@ -1,14 +1,11 @@
 from pytube         import YouTube
 from moviepy.editor import VideoFileClip
-from os             import path, remove, listdir
+from os             import path, remove
 
 def download(url):
-    for file in listdir("dls"):
-        remove("dls/" + file)
-
     yt          = YouTube(url)
     stream      = yt.streams.get_highest_resolution ()
-    vid_path    = stream    .download               (output_path="dls")
+    vid_path    = stream    .download               ()
 
     name, extension = path.splitext(vid_path)
     if not extension == ".mp4":
